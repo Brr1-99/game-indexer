0705e8db12f0063a3311f37fe23f7ef62a62920a
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2023-08-02 13:11:13 +0200

    Extract Cards

---
+++ src/lib/components/GameCard.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import type { GameDto } from "$lib/types"
+
+    export let game: GameDto
+</script>
+
+<div class="rounded bg-zinc-700 p-2">
+    <p class="pb-4">
+        <i class="bi bi-person-fill" />
+        {game.name}
+    </p>
+</div>
---
+++ src/lib/components/OwnerCard.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import type { OwnerDto } from '$lib/types'
+
+    export let owner: OwnerDto
+</script>
+
+<div class="rounded bg-zinc-700 p-2">
+    <p class="mb-2 text-center text-xl flex gap-2">
+        <img class="rounded" src={owner.imageUrl} alt="avatar {owner.name}" width="48px" />
+        <span>{owner.name}</span>
+    </p>
+
+    <p class="text-center">games: {owner.games.length}</p>
+</div>
