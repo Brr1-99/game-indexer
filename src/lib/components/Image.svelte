11818a958f2b555c0713460df69b624e3985f7d9
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2023-08-04 11:39:38 +0200

    Image components

---
+++ src/lib/components/Image.svelte
@@ -0,0 +1,24 @@
+<!--
+  @component
+  ## Image
+  Display an image using `background-image` instead of `<img/>`
+  @param image - the URL to the image
+-->
+<script lang="ts">
+    export let src: string
+
+    const style = 'background-image: url(' + src + ');'
+</script>
+
+<div class="rounded bgimage h-full w-full" {style} />
+
+<style>
+    .bgimage {
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+        height: 100%;
+        width: 100%;
+        overflow: hidden;
+    }
+</style>
